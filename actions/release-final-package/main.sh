7d3a24db69fbc910bf219e429efc5d693316ff9e
ehhuang <ehhuang@users.noreply.github.com>
2025-05-27 13:19:47 -0700

    fix: remove .bak when updating (#28)
    # What does this PR do?
    
    
    ## Test Plan

--- actions/release-final-package/main.sh
+++ actions/release-final-package/main.sh
@@ -89,7 +89,7 @@ add_bump_version_commit() {
       perl -pi -e "s/llama-stack-client>=.*,/llama-stack-client>=$RELEASE_VERSION\",/" pyproject.toml
 
       if [ "$repo" == "stack" ]; then
-        sed -i.bak -E 's/("llama-stack-client": ")[^"]+"/\1'"$RELEASE_VERSION"'"/' llama_stack/ui/package.json
+        sed -i -E 's/("llama-stack-client": ")[^"]+"/\1'"$RELEASE_VERSION"'"/' llama_stack/ui/package.json
       fi
 
       if [ -f "src/llama_stack_client/_version.py" ]; then
