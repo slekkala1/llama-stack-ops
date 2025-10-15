971554107aaed6726c2a9ec1f5b1f66ce36f369e
Swapna Lekkala <swapna942@meta.com>
2025-10-15 14:38:32 -0700

    fix: test and cut

--- actions/test-and-cut/main.sh
+++ actions/test-and-cut/main.sh
@@ -135,7 +135,6 @@ test_docker() {
     -e OLLAMA_URL=http://localhost:11434 \
     -e SAFETY_MODEL=ollama/llama-guard3:1b \
     -e LLAMA_STACK_TEST_INFERENCE_MODE=replay \
-    -e LLAMA_STACK_TEST_RECORDING_DIR=/app/llama-stack-source/tests/integration/recordings \
     -e TOGETHER_API_KEY=$TOGETHER_API_KEY \
     -e FIREWORKS_API_KEY=$FIREWORKS_API_KEY \
     -e TAVILY_SEARCH_API_KEY=$TAVILY_SEARCH_API_KEY \
